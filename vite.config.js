5e18f8d83ffacda4e74886a90a2be4b8eeb38c73
neo091 <meo666@gmail.com>
2026-01-27 23:27:09 +0100

    fix rrd version

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/taxi/", // <-- Esto es clave
+  // base: "/taxi/", // <-- Esto es clave
 })
